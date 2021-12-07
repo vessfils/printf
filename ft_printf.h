bd228fb6779511f337123690193c06681a569ed2
Jean-Francois <73600690+vessfils@users.noreply.github.com>
2021-12-07 20:20:01 +0100

    Add files via upload

---
+++ ft_printf.h
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_printf.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jcampagn <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/12/07 18:09:37 by jcampagn          #+#    #+#             */
+/*   Updated: 2021/12/07 18:11:01 by jcampagn         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FT_PRINTF_H
+# define FT_PRINTF_H
+
+# include <stdarg.h>
+# include <unistd.h>
+# include <stdio.h>
+
+int	ft_printf(const char*str, ...);
+#endif
